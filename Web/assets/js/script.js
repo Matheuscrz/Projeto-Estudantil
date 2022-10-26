b2453a5cdf6e497780d44b6ff9badbc0392f9203
Matheus Lima Da Cruz <matheuslimasof.eng@gmail.com>
2022-10-26 10:27:58 -0300

    Update

--- Web/assets/js/script.js
+++ Web/assets/js/script.js
@@ -1,9 +1,11 @@
 //Variáveis de construção do gráfico
-const label = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'] //Faixa de Pontuação
-const vagas = [1200, 1900, 300, 500, 200, 300] //Número de Vagas
+const label = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']; //Faixa de Pontuação
+const vagas = [1200, 1900, 300, 500, 200, 300]; //Número de Vagas
+const curse = 'temp'
 
 //TypeHead
-var substringMatcher = function(strs) {
+const type = () => {
+  var substringMatcher = function(strs) {
     return function findMatches(q, cb) {
       var matches, substringRegex;
   
@@ -41,49 +43,50 @@ var substringMatcher = function(strs) {
     name: 'states',
     source: substringMatcher(states)
   });
-
-
+} 
 
 //Calculo de intervalos
 const calculo = (array) => {
-    const k = 1 + 3.3*Math.log(array.length()) //Número de intervalos 
-    const At = Math.max(array) - Math.min(array) //Amplitude total
-    return (At/k).toFixed(2) //Retorna a amplitude de cada intervalo
+    const k = 1 + 3.3*Math.log(array.length()); //Número de intervalos 
+    const At = Math.max(array) - Math.min(array); //Amplitude total
+    return (At/k).toFixed(2); //Retorna a amplitude de cada intervalo
 }
-
 // Construção do gráfico
-let ctx = document.getElementById('chart').getContext('2d')
-let chart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: label,
-        datasets: [{
-            label: 'Candidatos por faixa de Pontuação',
-            data: vagas,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+const grafico = () =>{
+  let ctx = document.getElementById('myChart').getContext('2d');
+  let chat = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: label,
+          datasets: [{
+              label: 'Número de candidatos por faixa de Pontuação',
+              data: vagas,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1))'
+              ],
+              borderWidth: 1,
+              borderColor:'#777',
+              hoverBorderWidth: 3,
+              hoverBorderColor:'#000'
+          }]
+      },
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
+  });
+}
+grafico()
+//Função que coletara os dados e irar gerar os gráficos
+const process = () => {
+  var input = document.getElementById('input').value;
+
+}

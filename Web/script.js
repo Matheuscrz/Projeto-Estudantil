b44be38cc692dc329bfe0d16f8787e9ffeab72c2
Matheus Lima Da Cruz <matheuslimasof.eng@gmail.com>
2022-11-02 18:51:24 -0300

    Finalização e correção de bugs - Interface web

--- Web/script.js
+++ Web/script.js
@@ -1,48 +1,26 @@
-//Variáveis de construção do gráfico
-var label = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]; //Faixa de Pontuação
-var vagas = [1200, 1900, 300, 500, 200, 300]; //Número de Vagas
-
-//TypeHead
-$(document).ready(function () {
-  var substringMatcher = function (strs) {
-    return function findMatches(q, cb) {
-      var matches, substringRegex;
-
-      matches = [];
-
-      substrRegex = new RegExp(q, "i");
-
-      $.each(strs, function (i, str) {
-        if (substrRegex.test(str)) {
-          matches.push(str);
-        }
-      });
-
-      cb(matches);
-    };
-  };
-
-  $("div .typeahead").typeahead(
-    {
-      hint: true,
-      highlight: true,
-      minLength: 1,
-    },
-    {
-      name: "label",
-      source: substringMatcher(label),
-    }
-  );
-});
+//Variáveis de controle
+var label = []; //Faixa de Pontuação
+var nAluno = []; //Número de Alunos
+const curso = new Set(); //Objeto que recebe os cursos e elimina os repetidos
+const ListaCurso = []; //Lista de cursos para typehead
+const notas = []; //Armazena as notas do curso solicitado
 
 //Calculo de intervalos
-const calculo = (array) => {
-  const k = 1 + 3.3 * Math.log(array.length()); //Número de intervalos
-  const At = Math.max(array) - Math.min(array); //Amplitude total
-  return (At / k).toFixed(2); //Retorna a amplitude de cada intervalo
+const calculoIntervalo = () => {
+  const max = notas.reduce(function (prev, current) {
+    return prev > current ? prev : current;
+  });
+  const min = notas.reduce(function (prev, current) {
+    return prev < current ? prev : current;
+  });
+  return ((max - min) / notas.length).toFixed(2); //Calculo de distribuição de frequência
+};
+//Calculo do número de classes
+const nClasse = () => {
+  return Math.round(1 + 3.322 * Math.log(notas.length));
 };
 // Construção do gráfico
-const grafico = () => {
+const grafico = (x, y) => {
   let ctx = document.getElementById("myChart").getContext("2d");
   let chat = new Chart(ctx, {
     type: "bar",
@@ -51,7 +29,7 @@ const grafico = () => {
       datasets: [
         {
           label: "Número de candidatos por faixa de Pontuação",
-          data: vagas,
+          data: nAluno,
           backgroundColor: [
             "rgba(255, 99, 132, 1)",
             "rgba(54, 162, 235, 1)",
@@ -77,26 +55,103 @@ const grafico = () => {
   });
 };
 
-const test = () => {
-  // fetch("./Data/Data.json").then(async (res) => {
-  //   const data = await res.json();
-  // });
+//Função que cria um array para preenchimento de função do autocomplete
+async function logJson() {
+  const resposta = await fetch("./Data/Data.json");
+  const json = await resposta.json();
+  json.forEach((element) => {
+    curso.add(element.CURSO);
+  });
+  curso.forEach((element) => {
+    ListaCurso.push(element);
+  });
+}
+logJson();
+
+//Função que organiza as notas por faixa de pontuação
+const notaFaixa = () => {
+  let c1 = 0;
+  let c2 = 0;
+  let c3 = 0;
+  let c4 = 0;
+  let c5 = 0;
+  nAluno.length = 0;
+  let aux = notas.reduce(function (prev, current) {
+    return prev < current ? prev : current;
+  });
+  let array = [];
+  array.length = 0;
+  for (let i = 0; i < nClasse; i++) {
+    array.push(Number(aux));
+    aux += Number(calculoIntervalo());
+    console.log(aux);
+  }
+  console.log(array);
+  notas.forEach((element) => {});
+  // nAluno.push(c1, c2, c3, c4, c5);
 };
-//Função que coletara os dados e irar gerar os gráficos
+
+//Função que ira fazer a coleta de dados e executara todo o procedimento para geração do gráfico
 const process = () => {
   //Coleta de dados do Json
-  let cursos = [];
-  let notaCurso = [];
   var input = document.getElementById("input").value;
-  // fetch("./Data/Data.json").then(async (res) => {
-  //   const data = await res.json();
-  //   return Object.values(data);
-  // });
-  async function logJson() {
+  var curse = input.toUpperCase();
+  async function Data() {
     const resposta = await fetch("./Data/Data.json");
     const json = await resposta.json();
-    return json;
+    notas.length = 0;
+    json.forEach((element) => {
+      if (element.CURSO === curse) {
+        notas.push(element.NOTA);
+      }
+    });
   }
-  console.log(logJson());
-  grafico();
+  Data();
+  label.length = 0;
+  let aux = notas.reduce(function (prev, current) {
+    return prev < current ? prev : current;
+  });
+  while (label.length < nClasse) {
+    label.push(
+      `${Number(Math.round(aux))} - ${
+        Number(Math.round(aux)) + Math.round(Number(calculoIntervalo()))
+      }`
+    );
+    aux += Number(calculoIntervalo());
+  }
+  notaFaixa();
+  // grafico(label, nAluno);
 };
+
+//TypeHead
+$(document).ready(function () {
+  var substringMatcher = function (strs) {
+    return function findMatches(q, cb) {
+      var matches, substringRegex;
+
+      matches = [];
+
+      substrRegex = new RegExp(q, "i");
+
+      $.each(strs, function (i, str) {
+        if (substrRegex.test(str)) {
+          matches.push(str);
+        }
+      });
+
+      cb(matches);
+    };
+  };
+
+  $("div .typeahead").typeahead(
+    {
+      hint: false,
+      highlight: true,
+      minLength: 1,
+    },
+    {
+      name: "curso",
+      source: substringMatcher(ListaCurso),
+    }
+  );
+});

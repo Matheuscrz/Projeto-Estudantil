6c810e90bc17c48eb6bc0da2a84827799b065117
Matheus Lima Da Cruz <matheuslimasof.eng@gmail.com>
2022-11-03 19:20:13 -0300

    Versão Web

--- Web/script.js
+++ Web/script.js
@@ -13,12 +13,50 @@ const calculoIntervalo = () => {
   const min = notas.reduce(function (prev, current) {
     return prev < current ? prev : current;
   });
-  return ((max - min) / notas.length).toFixed(2); //Calculo de distribuição de frequência
+  return (max - min) / nClasse(); //Calculo de distribuição de frequência
 };
 //Calculo do número de classes
 const nClasse = () => {
-  return Math.round(1 + 3.322 * Math.log(notas.length));
+  return Math.round(Math.sqrt(notas.length));
 };
+//Função que processa os dados para montagem do gráfico
+const notaFaixa = () => {
+  nAluno.length = 0;
+  let sup = [];
+  let aux = notas.reduce((prev, current) => {
+    return prev < current ? prev : current;
+  });
+  let temp = aux;
+  for (let i = 0; i <= nClasse(); i++) {
+    sup.push(temp.toFixed(2));
+    temp = Number(aux) + Number(calculoIntervalo());
+    aux += Number(calculoIntervalo());
+  }
+  let intervalos = sup.map((str) => {
+    return Number(str);
+  });
+  for (let i = 1; i < intervalos.length; i++) {
+    label.push(`${intervalos[i - 1]} - ${intervalos[i]}`);
+  }
+  let arrayAux = [];
+  let array = [];
+  for (let i = 0; i < intervalos.length; i++) {
+    notas.forEach((element) => {
+      if (
+        element >= intervalos[i] &&
+        element < intervalos[i] + calculoIntervalo() - 0.1
+      ) {
+        arrayAux.push(element);
+      }
+    });
+    array.push(arrayAux);
+    arrayAux = [];
+  }
+  nAluno = array.map((x) => {
+    return x.length;
+  });
+};
+
 // Construção do gráfico
 const grafico = (x, y) => {
   let ctx = document.getElementById("myChart").getContext("2d");
@@ -68,29 +106,6 @@ async function logJson() {
 }
 logJson();
 
-//Função que organiza as notas por faixa de pontuação
-const notaFaixa = () => {
-  let c1 = 0;
-  let c2 = 0;
-  let c3 = 0;
-  let c4 = 0;
-  let c5 = 0;
-  nAluno.length = 0;
-  let aux = notas.reduce(function (prev, current) {
-    return prev < current ? prev : current;
-  });
-  let array = [];
-  array.length = 0;
-  for (let i = 0; i < nClasse; i++) {
-    array.push(Number(aux));
-    aux += Number(calculoIntervalo());
-    console.log(aux);
-  }
-  console.log(array);
-  notas.forEach((element) => {});
-  // nAluno.push(c1, c2, c3, c4, c5);
-};
-
 //Função que ira fazer a coleta de dados e executara todo o procedimento para geração do gráfico
 const process = () => {
   //Coleta de dados do Json
@@ -107,20 +122,8 @@ const process = () => {
     });
   }
   Data();
-  label.length = 0;
-  let aux = notas.reduce(function (prev, current) {
-    return prev < current ? prev : current;
-  });
-  while (label.length < nClasse) {
-    label.push(
-      `${Number(Math.round(aux))} - ${
-        Number(Math.round(aux)) + Math.round(Number(calculoIntervalo()))
-      }`
-    );
-    aux += Number(calculoIntervalo());
-  }
   notaFaixa();
-  // grafico(label, nAluno);
+  grafico(label, nAluno);
 };
 
 //TypeHead
